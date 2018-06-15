5d58736786944a07f3fed23170a4b38cf274811b
michaelstsnow <michael.st.snow@gmail.com>
2018-06-15 01:23:24 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,11 @@
 class Dog
-  def initialize(dog_breed)
+  def initialize(dog_name,dog_breed)
     if dog_breed
       @breed=dog_breed
     else
       @breed=Mutt
     end
-
+    @name=dog_name
   end
 
 end
-
-
-fido = Dog.new("Fido", "Pug")
